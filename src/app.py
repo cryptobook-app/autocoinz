bd98b691dde20c87a9d5afe60ea6ee4f6faec5bf
Robert Gloginja <robertgloginja@gmail.com>
2017-07-04 02:24:55 +1000

    Script Dev
    Primary script dev

--- src/app.py
+++ src/app.py
@@ -2,4 +2,27 @@ from bittrex import bittrex
 
 api = bittrex('', '')
 
-print api.getbalances()
+from terminaltables import AsciiTable
+table_data = [
+    ['Currency', 'Available', 'Pending', 'Balance']
+]
+table = AsciiTable(table_data)
+
+myBalances = api.getbalances()
+
+i = 0
+while i < len(myBalances):
+	checkthis = myBalances[i]['Available']
+
+	if checkthis == 0.0:
+		nothing = 0;
+	else:
+		NewAvailable = '{0:.8f}'.format(myBalances[i]['Available'])
+		NewPending = '{0:.8f}'.format(myBalances[i]['Pending'])
+		NewBalance = '{0:.8f}'.format(myBalances[i]['Balance'])
+		
+		table_data.append([myBalances[i]['Currency'], NewAvailable, NewPending , NewBalance])
+	
+	i += 1
+	
+print table.table
